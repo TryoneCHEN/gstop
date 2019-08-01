359da593db530e7353c8fccdaeb7a88ec2493b28
Your Name <you@example.com>
2019-08-01 23:12:36 +0800

    登录界面效果3

--- src/pages/Login/Login.vue
+++ src/pages/Login/Login.vue
@@ -14,7 +14,7 @@
             <div :class="{on:loginWay}">
               <section class="login_message">
                 <input type="tel" maxlength="11" placeholder="手机号" v-model="phone">
-                <button disabled="disabled" class="get_verification" :class="{right_phone: rightPhone}">获取验证码</button>
+                <button :disabled="!rightPhone" class="get_verification" :class="{right_phone: rightPhone}" @click.prevent="getCode">{{computeTime>0 ? `已发送(${computeTime}s)` : '获取验证码'}}</button>
               </section>
               <section class="login_verification">
                 <input type="tel" maxlength="8" placeholder="验证码">
@@ -60,12 +60,32 @@ export default {
   data () {
     return {
       loginWay: false, // true代表短信登陆, false代表密码
-      phone: '' // 手机号
+      phone: '', // 手机号
+      computeTime: 0 // 计时的时间
     }
   },
   computed: {
     rightPhone () {
       return /^1\d{10}$/.test(this.phone)
+    }
+  },
+  methods: {
+    // 异步获取短信验证码
+    getCode () {
+      // 如果当前没有计时
+      if (!this.computeTime) {
+        // 启动倒计时
+        this.computeTime = 30
+        this.intervalId = setInterval(() => {
+          this.computeTime--
+          if (this.computeTime <= 0) {
+            // 停止计时
+            clearInterval(this.intervalId)
+          }
+        }, 1000)
+
+        // 发送ajax请求(向指定手机号发送验证码短信)
+      }
     }
   }
 }
