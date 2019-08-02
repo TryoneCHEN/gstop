1e1b65505130bf7986c1b87b44dd52cb8d0a76d9
Your Name <you@example.com>
2019-08-02 08:46:24 +0800

    登录界面效果5表单验证

--- src/pages/Login/Login.vue
+++ src/pages/Login/Login.vue
@@ -10,14 +10,14 @@
           </div>
         </div>
         <div class="login_content">
-          <form>
+          <form @submit.prevent="login">
             <div :class="{on:loginWay}">
               <section class="login_message">
                 <input type="tel" maxlength="11" placeholder="手机号" v-model="phone">
                 <button :disabled="!rightPhone" class="get_verification" :class="{right_phone: rightPhone}" @click.prevent="getCode">{{computeTime>0 ? `已发送(${computeTime}s)` : '获取验证码'}}</button>
               </section>
               <section class="login_verification">
-                <input type="tel" maxlength="8" placeholder="验证码">
+                <input type="tel" maxlength="8" placeholder="验证码" v-model="code">
               </section>
               <section class="login_hint">
                 温馨提示：未注册硅谷外卖帐号的手机号，登录时将自动注册，且代表已同意
@@ -27,7 +27,7 @@
             <div :class="{on:!loginWay}">
               <section>
                 <section class="login_message">
-                  <input type="tel" maxlength="11" placeholder="手机/邮箱/用户名">
+                  <input type="tel" maxlength="11" placeholder="手机/邮箱/用户名" v-model="name">
                 </section>
                 <section class="login_verification">
                   <input type="tel" maxlength="8" placeholder="密码" v-if="showPwd" v-model="pwd">
@@ -38,7 +38,7 @@
                   </div>
                 </section>
                 <section class="login_message">
-                  <input type="text" maxlength="11" placeholder="验证码">
+                  <input type="text" maxlength="11" placeholder="验证码" v-model="captcha">
                   <img class="get_verification" src="./images/captcha.svg" alt="captcha">
                 </section>
               </section>
@@ -51,11 +51,13 @@
           <i class="iconfont icon-jiantou2"></i>
         </a>
       </div>
+      <AlertTip :alertText="alertText" v-show="alertShow" @closeTip="closeTip"/>
     </section>
   </div>
 </template>
 
 <script>
+import AlertTip from '../../components/AlertTip/AlertTip.vue'
 export default {
   name: 'login',
   data () {
@@ -64,6 +66,11 @@ export default {
       phone: '', // 手机号
       computeTime: 0, // 计时的时间
       showPwd: false, // 是否显示密码
+      captcha: '', // 图形验证码
+      alertText: '', // 提示文本
+      alertShow: false, // 是否显示警告框
+      code: '', // 短信验证码
+      name: '', // 用户名
       pwd: '' // 密码
     }
   },
@@ -89,9 +96,46 @@ export default {
 
         // 发送ajax请求(向指定手机号发送验证码短信)
       }
+    },
+    showAlert (alertText) {
+      this.alertShow = true
+      this.alertText = alertText
+    },
+    login () {
+      // 前台表单验证
+      if (this.loginWay) { // 短信登陆
+        const {rightPhone, phone, code} = this
+        if (!this.rightPhone) {
+          // 手机号不正确
+          this.showAlert('手机号不正确')
+        } else if (!/^\d{6}$/.test(code)) {
+          // 验证必须是6位数字
+          this.showAlert('验证必须是6位数字')
+        }
+      } else { // 密码登陆
+        const {name, pwd, captcha} = this
+        if (!this.name) {
+          // 用户名必须指定
+          this.showAlert('用户名必须指定')
+        } else if (!this.pwd) {
+          // 密码必须指定
+          this.showAlert('密码必须指定')
+        } else if (!this.captcha) {
+          // 验证码必须指定
+          this.showAlert('验证码必须指定')
+        }
+      }
+    },
+    closeTip () {
+      this.alertShow = false
+      this.alertText = ''
     }
+  },
+  components: {
+    AlertTip
   }
 }
+
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
