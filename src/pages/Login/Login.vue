4d6cba2098676e2ca3155de015849d7afed00523
Your Name <you@example.com>
2019-08-02 08:04:10 +0800

    登录界面效果4密码显示和隐藏

--- src/pages/Login/Login.vue
+++ src/pages/Login/Login.vue
@@ -30,10 +30,11 @@
                   <input type="tel" maxlength="11" placeholder="手机/邮箱/用户名">
                 </section>
                 <section class="login_verification">
-                  <input type="tel" maxlength="8" placeholder="密码">
-                  <div class="switch_button off">
-                    <div class="switch_circle"></div>
-                    <span class="switch_text">...</span>
+                  <input type="tel" maxlength="8" placeholder="密码" v-if="showPwd" v-model="pwd">
+                  <input type="password" maxlength="8" placeholder="密码" v-else v-model="pwd">
+                  <div class="switch_button" :class="showPwd?'on':'off'" @click="showPwd=!showPwd">
+                    <div class="switch_circle" :class="{right: showPwd}"></div>
+                    <span class="switch_text">{{showPwd ? 'abc' : '...'}}</span>
                   </div>
                 </section>
                 <section class="login_message">
@@ -61,7 +62,9 @@ export default {
     return {
       loginWay: false, // true代表短信登陆, false代表密码
       phone: '', // 手机号
-      computeTime: 0 // 计时的时间
+      computeTime: 0, // 计时的时间
+      showPwd: false, // 是否显示密码
+      pwd: '' // 密码
     }
   },
   computed: {
@@ -181,7 +184,6 @@ export default {
                   &.on
                     background #02a774
                   >.switch_circle
-                    //transform translateX(27px)
                     position absolute
                     top -1px
                     left -1px
@@ -192,6 +194,8 @@ export default {
                     background #fff
                     box-shadow 0 2px 4px 0 rgba(0,0,0,.1)
                     transition transform .3s
+                    &.right
+                      transform translateX(30px)
               .login_hint
                 margin-top 12px
                 color #999
