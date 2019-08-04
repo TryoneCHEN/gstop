b4b87855d33626ea6b352dce72ee2aa692ab9d5a
Your Name <you@example.com>
2019-08-04 11:31:50 +0800

    完成登录请求

--- src/pages/Login/Login.vue
+++ src/pages/Login/Login.vue
@@ -39,7 +39,7 @@
                 </section>
                 <section class="login_message">
                   <input type="text" maxlength="11" placeholder="验证码" v-model="captcha">
-                  <img class="get_verification" src="http://localhost:4000/captcha"  alt="captcha" @click="getCaptcha" refs="captcha">
+                  <img class="get_verification" src="http://localhost:4000/captcha"  alt="captcha" @click="getCaptcha" ref="captcha">
                 </section>
               </section>
             </div>
@@ -114,29 +114,59 @@ export default {
       this.alertText = alertText
     },
     // 异步登陆
-    login () {
+    async login () {
+      let result
       // 前台表单验证
       if (this.loginWay) { // 短信登陆
         const {rightPhone, phone, code} = this
         if (!this.rightPhone) {
           // 手机号不正确
           this.showAlert('手机号不正确')
+          return
         } else if (!/^\d{6}$/.test(code)) {
           // 验证必须是6位数字
           this.showAlert('验证必须是6位数字')
+          return
         }
+        // 发送ajax请求短信登陆
+        result = await reqSmsLogin(phone, code)
       } else { // 密码登陆
         const {name, pwd, captcha} = this
         if (!this.name) {
           // 用户名必须指定
           this.showAlert('用户名必须指定')
+          return
         } else if (!this.pwd) {
           // 密码必须指定
           this.showAlert('密码必须指定')
+          return
         } else if (!this.captcha) {
           // 验证码必须指定
           this.showAlert('验证码必须指定')
+          return
         }
+        // 发送ajax请求密码登陆
+        result = await reqPwdLogin({name, pwd, captcha})
+      }
+      // 停止计时
+      if (this.computeTime) {
+        this.computeTime = 0
+        clearInterval(this.intervalId)
+        this.intervalId = undefined
+      }
+      // 根据结果数据处理
+      if (result.code === 0) {
+        const user = result.data
+        // 将user保存到vuex的state
+        this.$store.dispatch('recordUser', user)
+        // 去个人中心界面
+        this.$router.replace('/profile')
+      } else {
+        // 显示新的图片验证码
+        this.getCaptcha()
+        // 显示警告提示
+        const msg = result.msg
+        this.showAlert(msg)
       }
     },
     // 关闭警告框
@@ -147,7 +177,7 @@ export default {
     // 获取一个新的图片验证码
     getCaptcha () {
       // 每次指定的src要不一样
-      this.$refs.captcha.src = 'http://localhost:4000/captcha?time='+Date.now()
+      this.$refs.captcha.src = 'http://localhost:4000/captcha?time=' + Date.now()
       // 法二
       // this.$refs.captcha.src = 'http://localhost:4000/captcha?time='+Math.random()
     }
