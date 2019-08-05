bff58716da2842cdf55b4315decf97a80ed6cf70
Your Name <you@example.com>
2019-08-05 14:23:19 +0800

    自动登录

--- src/store/actions.js
+++ src/store/actions.js
@@ -11,7 +11,8 @@ import {
 import {
   reqAddress,
   reqFoodCategorys,
-  reqShops
+  reqShops,
+  reqUserInfo
 } from '../api'
 
 export default {
@@ -52,5 +53,13 @@ export default {
   // 同步记录用户信息
   recordUser ({commit}, userInfo) {
     commit(RECEIVE_USER_INFO, {userInfo})
+  },
+  // 异步获取用户信息
+  async getUserInfo ({commit}) {
+    const result = await reqUserInfo()
+    if (result.code === 0) {
+      const userInfo = result.data
+      commit(RECEIVE_USER_INFO, {userInfo})
+    }
   }
 }
