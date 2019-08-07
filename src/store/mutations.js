1bf8553594d72a963dfea2af41bc55da2a4c9ef2
Your Name <you@example.com>
2019-08-07 18:57:55 +0800

    cartcontrol组件2

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -1,6 +1,7 @@
 /*
 直接更新state的多个方法的对象
  */
+import Vue from 'vue'
 import {
   RECEIVE_ADDRESS,
   RECEIVE_CATEGORYS,
@@ -9,7 +10,9 @@ import {
   RECEIVE_INFO,
   RECEIVE_RATINGS,
   RECEIVE_GOODS,
-  RECEIVE_SHOPS
+  RECEIVE_SHOPS,
+  INCREMENT_FOOD_COUNT,
+  DECREMENT_FOOD_COUNT
 } from './mutation-types'
 
 export default {
@@ -29,15 +32,39 @@ export default {
   [RECEIVE_SHOPS] (state, {shops}) {
     state.shops = shops
   },
-  [RECEIVE_INFO](state, {info}) {
+  [RECEIVE_INFO] (state, {info}) {
     state.info = info
   },
 
-  [RECEIVE_RATINGS](state, {ratings}) {
+  [RECEIVE_RATINGS] (state, {ratings}) {
     state.ratings = ratings
   },
 
-  [RECEIVE_GOODS](state, {goods}) {
+  [RECEIVE_GOODS] (state, {goods}) {
     state.goods = goods
+  },
+  [INCREMENT_FOOD_COUNT] (state, {food}) {
+    if (!food.count) { // 第一次增加
+      // food.count = 1  // 新增属性(没有数据绑定)
+      /*
+      对象
+      属性名
+      属性值
+       */
+      Vue.set(food, 'count', 1) // 让新增的属性也有数据绑定
+      // 将food添加到cartFoods中
+      // state.cartFoods.push(food)
+    } else {
+      food.count++
+    }
+  },
+  [DECREMENT_FOOD_COUNT] (state, {food}) {
+    if (food.count) { // 只有有值才去减
+      food.count--
+      // if (food.count === 0) {
+      //   // 将food从cartFoods中移除
+      //   state.cartFoods.splice(state.cartFoods.indexOf(food), 1)
+      // }
+    }
   }
 }
