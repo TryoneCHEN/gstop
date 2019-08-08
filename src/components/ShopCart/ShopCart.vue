af12b20fbffe0241524d3d44c5aaeab96b7bf14c
Your Name <you@example.com>
2019-08-08 10:49:10 +0800

    显示购物项列表

--- src/components/ShopCart/ShopCart.vue
+++ src/components/ShopCart/ShopCart.vue
@@ -2,7 +2,7 @@
   <div>
     <div class="shopcart">
       <div class="content">
-        <div class="content-left">
+        <div class="content-left" @click="toggleShow">
           <div class="logo-wrapper">
             <div class="logo" :class="{highlight: totalCount}">
               <i class="iconfont icon-shopping_cart" :class="{highlight: totalCount}"></i>
@@ -19,18 +19,18 @@
         </div>
       </div>
       <transition name="move">
-        <div class="shopcart-list" style="display: none;">
+        <div class="shopcart-list" v-show="isShow" @click="toggleShow">
           <div class="list-header">
             <h1 class="title">购物车</h1>
             <span class="empty">清空</span>
           </div>
           <div class="list-content">
             <ul>
-              <li class="food">
-                <span class="name">红枣山药糙米粥</span>
-                <div class="price"><span>￥10</span></div>
+              <li class="food" v-for="(food, index) in cartFoods" :key="index">
+                <span class="name">{{food.name}}</span>
+                <div class="price"><span>￥{{food.price}}</span></div>
                 <div class="cartcontrol-wrapper">
-                  <div class="cartcontrol"> <div class="iconfont icon-remove_circle_outline"></div> <div class="cart-count">1</div> <div class="iconfont icon-add_circle"></div> </div>
+                  <CartControl :food="food"/>
                 </div>
               </li>
             </ul>
@@ -45,7 +45,21 @@
 
 <script>
 import {mapState, mapGetters} from 'vuex'
+import CartControl from '../CartControl/CartControl.vue'
 export default {
+  data () {
+    return {
+      isShow: false
+    }
+  },
+  methods: {
+    toggleShow () {
+      // 只有当总数量大于0时切换
+      if (this.totalCount > 0) {
+        this.isShow = !this.isShow
+      }
+    }
+  },
   computed: {
     ...mapState(['cartFoods', 'info']),
     ...mapGetters(['totalCount', 'totalPrice']),
@@ -66,6 +80,9 @@ export default {
         return '结算'
       }
     }
+  },
+  components: {
+    CartControl
   }
 }
 </script>
