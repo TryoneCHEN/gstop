7844925bd3bba1e857311350afe4bc296c4a9f5e
Your Name <you@example.com>
2019-08-08 23:05:49 +0800

    路由懒加载

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,27 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 // import HelloWorld from '@/components/HelloWorld'
-import Msite from '../pages/Msite/Msite.vue'
-import Search from '../pages/Search/Search.vue'
-import Order from '../pages/Order/Order.vue'
-import Profile from '../pages/Profile/Profile.vue'
-import Login from '../pages/Login/Login.vue'
+// import Msite from '../pages/Msite/Msite.vue'
+// import Search from '../pages/Search/Search.vue'
+// import Order from '../pages/Order/Order.vue'
+// import Profile from '../pages/Profile/Profile.vue'
 
-import Shop from '../pages/Shop/Shop.vue'
-import ShopGoods from '../pages/Shop/ShopGoods/ShopGoods.vue'
-import ShopRatings from '../pages/Shop/ShopRatings/ShopRatings.vue'
-import ShopInfo from '../pages/Shop/ShopInfo/ShopInfo.vue'
+const Msite = () => import('../pages/Msite/Msite.vue')
+const Search = () => import('../pages/Search/Search.vue')
+const Order = () => import('../pages/Order/Order.vue')
+const Profile = () => import('../pages/Profile/Profile.vue')
+
+// import Login from '../pages/Login/Login.vue'
+// import Shop from '../pages/Shop/Shop.vue'
+// import ShopGoods from '../pages/Shop/ShopGoods/ShopGoods.vue'
+// import ShopRatings from '../pages/Shop/ShopRatings/ShopRatings.vue'
+// import ShopInfo from '../pages/Shop/ShopInfo/ShopInfo.vue'
+const Login = () => import('../pages/Login/Login.vue')
+const Shop = () => import('../pages/Shop/Shop.vue')
+const ShopGoods = () => import('../pages/Shop/ShopGoods/ShopGoods.vue')
+const ShopRatings = () => import('../pages/Shop/ShopRatings/ShopRatings.vue')
+const ShopInfo = () => import('../pages/Shop/ShopInfo/ShopInfo.vue')
+
 Vue.use(Router)
 
 export default new Router({
